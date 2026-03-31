2021404ed9c6a2d3cba3c8842614c9098bf02f1f
cyberblicc-sketch <cyberblicc@icloud.com>
2026-03-30 20:30:37 -0500

    Add relayer app module

---
+++ apps/relayer/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RelayController } from './relay.controller';
+import { RelayService } from './relay.service';
+import { GelatoProvider } from './providers/gelato.provider';
+import { ComplianceModule } from './compliance/compliance.module';
+import { PrismaModule } from '@arena/shared-prisma';
+import { HealthController } from './health.controller';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ComplianceModule, PrismaModule],
+  controllers: [RelayController, HealthController],
+  providers: [RelayService, GelatoProvider],
+})
+export class AppModule {}
